5f4cb978b523cef69386ca2417cd1e3e3a671b1c
Akex <67469268+akex06@users.noreply.github.com>
2023-04-23 16:39:17 +0200

    Delete obj directory

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TestPlugin
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CrabGameServerUtils";
-        public const string PLUGIN_NAME = "CrabGameServerUtils";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
